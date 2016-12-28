19e7b1270559b13a85549e96a43c871117c83ece
Gabriel Cencic <gabriel.cencic@gmail.com>
2016-12-28 00:07:35 -0200

    Initial bot with basic functions

---
+++ README.md
@@ -0,0 +1,25 @@
+# AnotherChatterBot Test Bot
+
+This is a simple test for my learning about Facebook bots
+
+## Overview
+
+This is a very simple bot created using [Calamars proto-framework](https://www.npmjs.com/package/calamars)
+
+The bot responds to greetings, farewell, compliments and can (barely) tell the weather. Also, he plays marcopolo.
+
+The bot uses Wit.ai for natural language understanding for a very few intentions.
+
+It uses [weather-js](https://www.npmjs.com/package/weather-js) to get the forecast using msn weather service and [dotenv](https://www.npmjs.com/package/dotenv) for loading environment variables.
+
+## Showcase
+
+The bot is implemented at facebook page [Just Another ChatterBot](https://www.facebook.com/justanotherchatterbot/), but it's offline most of the time since i'm using ngrok to secure tunnels for my localhost.
+
+## Issues
+
+The weather system is currently not fully developed, instead, the bot only answer a default message.
+
+## License
+
+[MIT][license]
---
+++ src/index.js
@@ -0,0 +1,116 @@
+const DotEnv = require('dotenv').config();
+const FacebookMessengerBot = require('calamars').FacebookMessengerBot;
+import { WitDriver, createRouter } from 'calamars';
+const Weather = require('weather-js');
+
+const myPageToken = process.env.FB_PAGE_TOKEN;
+const myVerifyToken = process.env.FB_VERIFY_TOKEN;
+const myCallbackPath = '/webhook';
+const myPort = process.env.PORT;
+const myAppSecret = process.env.FB_APP_SECRET;
+
+const myWitAppID = process.env.WIT_APP_ID;
+const myWitServerToken =process.env.WIT_SERVER_TOKEN;
+
+const witOptions = {
+    id: myWitAppID,
+    serverToken: myWitServerToken
+};
+
+const callbacks = {
+    marco() { return 'Polo!'; },
+    greetings() {
+      var sentences = ['Greetings :)', 'Hello!', 'Hi :D'];
+      var randomNumber = Math.floor((Math.random() * sentences.length));
+      return sentences[randomNumber];
+    },
+    goodbye() {return 'Have a nice week :D'},
+    gratitude() {return 'You are welcome :)'},
+    getWeather(location) {
+        //console.log(s);
+        if (undefined != location || null != location) {
+          //Here we call the weather API
+
+          //Note: I haven't found yet an API that returns a Promise
+          //Lets make a callback hell :(
+          function weatherCall(location, callback) {
+            Weather.find({search: 'San Francisco, CA', degreeType: 'C'}, function(err, result) {
+              if(err) console.log(err);
+              callback(result);
+            });
+          }
+          weatherCall(location, function(response){
+              console.log(response); //Ok i can get the weather...but...how do i return it?
+          });
+
+          //Since i can't get the weather from here, return a standard message
+          return "You asked for the forecast in " + location[0].value + ". It is cloudy. (That's a guess, i really should call a weather API here hehe)"; //Here we call API
+        }
+        else
+            return "Where? (I still have problems understanding flows, so please retype the entire sentence)"
+    }
+};
+
+const routes = [
+    [
+        payload => payload.entities.intent[0].value === 'greetings',
+        () => callbacks.greetings()
+    ],
+    [
+        payload => payload.entities.intent[0].value === 'marcopolo',
+        () => callbacks.marco()
+    ],
+    [
+        payload => payload.entities.intent[0].value === 'goodbye',
+        () => callbacks.goodbye()
+    ],
+    [
+        payload => payload.entities.intent[0].value === 'gratitude',
+        () => callbacks.gratitude()
+    ],
+    [
+        payload => payload.entities.intent[0].value === 'weather',
+        (payload) => callbacks.getWeather(payload.entities.location)
+    ]
+];
+
+const router = createRouter(routes);
+
+const myMessageListener = function(updateEvent){
+    // output received message
+    console.log('received update:', JSON.stringify(updateEvent.update, ' ', 2));
+
+    //Query wit
+    const queryPromise = wit.query(updateEvent.update.message.text)
+            .then(result => {
+                const { _text, outcomes } = result;
+                //console.log(outcomes[0].entities);  //entities
+                //Reply
+                const intentName = outcomes[0].entities.intent[0].value;
+                const confidence = outcomes[0].entities.intent[0].confidence;
+                updateEvent.bot.sendMessage({
+                    userId: updateEvent.update.sender.id,
+                    text: router({
+                              query: outcomes[0]._text,
+                              entities: outcomes[0].entities
+                    })
+                });
+            });
+};
+
+const wit = new WitDriver(witOptions);
+
+const mybot = new FacebookMessengerBot({
+    port: myPort,
+    callbackPath: myCallbackPath,
+    verifyToken: myVerifyToken,
+    pageTokens: [myPageToken],
+    appSecret: myAppSecret,
+    listeners: {
+        onMessage: myMessageListener
+    }
+});
+
+mybot.start().then(function(){
+    console.log(`Server is running on port ${myPort}`);
+})
